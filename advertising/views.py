e5dc427ce29404f7d5606fe5d57e88a99dab2c48
midhun98 <midhunskani@gmail.com>
2024-06-06 12:11:47 +0530

    Initial

--- advertising/views.py
+++ advertising/views.py
@@ -1,4 +1,8 @@
+from django.db.models import Sum
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Advertiser, Ad, Location, Transaction
 from .serializers import AdvertiserSerializer, AdSerializer, LocationSerializer, TransactionSerializer
 
@@ -13,6 +17,15 @@ class AdvertiserViewSet(viewsets.ModelViewSet):
 class AdViewSet(viewsets.ModelViewSet):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
+
+    @action(detail=False, methods=['get'])
+    def ad_spend_location(self, request, *args, **kwargs):
+        location = request.query_params.get('location')
+        if not location:
+            return Response({'detail': 'location query parameter is required.'}, status=400)
+
+        total_ad_spend = Ad.objects.filter(location__name=location).aggregate(total_spend=Sum('spend'))
+        return Response({'total_ad_spend': total_ad_spend})
 
 
 class LocationViewSet(viewsets.ModelViewSet):
