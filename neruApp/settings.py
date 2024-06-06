21367cd74067b9e48e2c66cbd564711961fc65ba
midhun98 <midhunskani@gmail.com>
2024-06-06 12:18:55 +0530

    Initial

--- neruApp/settings.py
+++ neruApp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'advertising'
+    'advertising',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
